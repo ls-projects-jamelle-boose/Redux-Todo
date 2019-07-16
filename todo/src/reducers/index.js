dd08be90d8c894dc74cfe3cfcc6a8a2340a50810
Jamelle-Boose <19831942+Jamelle-Boose@users.noreply.github.com>
2019-07-15 21:17:06 -0700

    add reducer function

---
+++ todo/src/reducers/index.js
@@ -0,0 +1,34 @@
+import { ADD_TODO } from "../actions";
+
+//Create store and initial list of todo items
+const initialState = {
+  title: "Todo List",
+  todoItems: [
+    { todoItem: "Walk the dog", completed: false, id: 3453543 },
+    { todoItem: "Take out the trash", completed: false, id: 12321312 }
+  ]
+};
+
+//Create reducer function and pass into createStore
+
+function reducer(state = initialState, action) {
+  console.log("insider reducer", action);
+  {
+    switch (action.type) {
+      case ADD_TODO:
+        const newTodoItem = {
+          todoItem: action.payload,
+          id: Date.now(),
+          completed: false
+        };
+        return {
+          ...state,
+          todoItems: [...state.todoItems, newTodoItem]
+        };
+      default:
+        return state;
+    }
+  }
+}
+
+export default reducer;
